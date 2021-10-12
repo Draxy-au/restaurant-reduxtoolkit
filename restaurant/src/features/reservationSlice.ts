0cb2345f49697afae9893ff1564d27faacea91a2
William Hamilton <draxy80@gmail.com>
2021-10-13 04:12:38 +1000

    created the addReservation reducer, that takes in a state and action and pushes (cause its an array) the payload on to the state. exporting the reducer with toolkit slice actions

--- restaurant/src/features/reservationSlice.ts
+++ restaurant/src/features/reservationSlice.ts
@@ -1,13 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
-const initialState = {
+//typescript wants to know the type of object initialState is, so we make an interface with typing for this
+interface ReservationState {
+    value: string[]
+}
+
+const initialState: ReservationState = {
     value: []
 }
 
 export const reservationSlice = createSlice({
     name: "reservations",
     initialState,
-    reducers: {}
+    reducers: {
+        //typescript wants to know the type of action, toolkit has the type PayloadAction, and we use string
+        addReservation: (state, action: PayloadAction<string>) => {
+            state.value.push(action.payload)    
+        }
+    }
 })
+
+export const { addReservation } = reservationSlice.actions;
 
 export default reservationSlice.reducer;
