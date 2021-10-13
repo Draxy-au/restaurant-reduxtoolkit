e585c1b8f0052006c4542c7d21dadf8b0f5de273
William Hamilton <draxy80@gmail.com>
2021-10-13 11:07:13 +1000

    completed with slices setup, actions and reducers handled much easier. I like it

--- restaurant/README.md
+++ restaurant/README.md
@@ -1,3 +1,5 @@
 Created with CRA --template typescript
 
 Going to try to use typescript where I can as I need to learn it.
+
+Following: https://www.youtube.com/watch?v=jR4fagDcvrc
--- restaurant/src/App.tsx
+++ restaurant/src/App.tsx
@@ -3,13 +3,17 @@ import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
 import { RootState } from "./app/store";
 import ReservationCard from "./components/ReservationCard";
+import CustomerCard from "./components/customerCard";
 import { addReservation } from "./features/reservationSlice";
 
 function App() {
-
   const [reservationNameInput, setReservationNameInput] = useState("");
 
-  const reservations = useSelector((state: RootState) => state.reservations.value);
+  const reservations = useSelector(
+    (state: RootState) => state.reservations.value
+  );
+
+  const customers = useSelector((state: RootState) => state.customers.value);
 
   const dispatch = useDispatch();
 
@@ -17,7 +21,7 @@ function App() {
     if (!reservationNameInput) return;
     dispatch(addReservation(reservationNameInput));
     setReservationNameInput("");
-  }
+  };
 
   return (
     <div className="App">
@@ -32,23 +36,17 @@ function App() {
             </div>
           </div>
           <div className="reservation-input-container">
-            <input 
-              value={reservationNameInput} onChange={(e) => setReservationNameInput(e.target.value)}
+            <input
+              value={reservationNameInput}
+              onChange={(e) => setReservationNameInput(e.target.value)}
             />
             <button onClick={hangleAddReservations}>Add</button>
           </div>
         </div>
         <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
-              </div>
-            </div>
-          </div>
+          {customers.map((customer) => {
+            return <CustomerCard id={customer.id} name={customer.name} food={customer.food} />;
+          })}
         </div>
       </div>
     </div>
